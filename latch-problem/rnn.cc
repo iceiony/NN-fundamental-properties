8c8537887bbfe17371d3fe7c677cd9b033b20f0d
Adrian Ionita <iceiony@yahoo.com>
2021-03-15 19:59:14 +0000

    train for different rates and save the training errors

--- latch-problem/rnn.cc
+++ latch-problem/rnn.cc
@@ -8,7 +8,10 @@ using namespace dy;
 
 #include <tuple>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <stdexcept>
+#include <math.h>
 
 using namespace std;
 
@@ -16,36 +19,37 @@ using namespace std;
 std::tuple<unsigned, unsigned, unsigned> read_args(int argc, char* argv[]){
     if(argc < 4) {
         std::cout << "Arguments expected for: \n";
-        std::cout << " * number of neurons in recurrent layer \n";
+        std::cout << " * network size \n";
         std::cout << " * the max length of noise \n";
         std::cout << " * number of training itterations \n";
 
         throw std::invalid_argument("Incorrect number of arguments.");
     }
 
-    unsigned neuron_count = std::atoi(argv[1]);
-    unsigned L_max = std::atoi(argv[2]);
+    unsigned size = std::atoi(argv[1]);
+    unsigned noise_len = std::atoi(argv[2]);
     unsigned epoch_max = std::atoi(argv[3]);
 
-    std::cout << " * number of neurons in recurrent layer : " <<  neuron_count << "\n";
-    std::cout << " * the max length of noise              : " << L_max << "\n";
+    std::cout << " * number of neurons in recurrent layer : " <<  size << "\n";
+    std::cout << " * the max length of noise              : " << noise_len << "\n";
     std::cout << " * number of training itterations       : " << epoch_max <<  "\n";
 
-    return std::make_tuple(neuron_count, L_max, epoch_max);
+    return std::make_tuple(size, noise_len, epoch_max);
 }
 
-void train(
+vector<dy::real> train(
         const vector<dy::real> X, 
         const vector<dy::real> Y, 
-        const unsigned neuron_count, 
-        const unsigned L, 
-        const unsigned epoch_max)
-{
+        const float rate, 
+        const unsigned size, 
+        const unsigned noise_len, 
+        const unsigned epoch_max
+){
     ComputationGraph cg;
-    RNN net(neuron_count);
-    SimpleSGDTrainer trainer(net.params, 0.05);
+    RNN net(size);
+    SimpleSGDTrainer trainer(net.params, rate);
 
-    Noise rng(-1.0, 1.0);
+    Noise rng(-2.0, 2.0);
 
     vector<dy::real> out(epoch_max);
     unsigned batch = (unsigned) Y.size();
@@ -53,8 +57,8 @@ void train(
     for(int epoch = 0; epoch < epoch_max; epoch ++){
         cg.clear();
 
-        auto X_ = rng.create_data(X, L);
-        auto x = input(cg, Dim({L + 1}, batch), &X_); 
+        auto X_ = rng.create_data(X, noise_len);
+        auto x = input(cg, Dim({noise_len + 1}, batch), &X_); 
         auto y = input(cg, Dim({1}, batch), &Y);
 
         auto y_pred = net.forward(cg, x);
@@ -71,8 +75,8 @@ void train(
         out[epoch] = my_loss;
     }
 
-    cout << "L = " << L << " - ";
-    cout << "E = " << out[0] << " " << out[epoch_max - 1] << endl;
+    //cout << "E = " << out[0] << " " << out[epoch_max - 1] << endl;
+    return(out);
 }
 
 
@@ -80,12 +84,35 @@ int main(int argc, char* argv[]){
     //int argc; char** argv;
     dy::initialize(argc, argv);
 
-    auto [neuron_count, L_max, epoch_max] = read_args(argc, argv);
-    //unsigned int neuron_count, L, epoch_max; tie(neuron_count, L, epoch_max) = make_tuple(10, 5 , 500);
+    auto [size, noise_len, epoch_max] = read_args(argc, argv);
+    //unsigned int size, noise_len, epoch_max; tie(size, noise_len, epoch_max) = make_tuple(10, 5 , 500);
 
     const vector<dy::real> X = {-1.0, 1.0};
     const vector<dy::real> Y = {0.0, 1.0};
-    for(unsigned L = 1; L <= L_max; L++){
-        train(X, Y, neuron_count, L, epoch_max);
+
+    unsigned success;
+    for(float rate = 3.55; rate > 0.0; rate -= 0.25){
+        stringstream file_path; 
+        ofstream error_log;
+
+        // rates, size, activation, 
+        file_path << "./out/" << noise_len << '_' << size << '_' << rate << ".csv"; 
+        error_log.open(file_path.str());
+
+        success = 0;
+        for(unsigned i = 0; i < 100; i++){
+            auto errs = train(X, Y, rate, size, noise_len, epoch_max);
+
+            if(errs[epoch_max - 1] < 0.18) success++;
+
+            for(auto e: errs)
+                error_log << e << ',';
+
+            error_log << endl;
+        }
+
+        cout << "rate: " << rate << " success: " << success << "%" << endl;
+
+        error_log.close();
     }
 }
